d977986cad5fe4991f6ec76060bb51524db86310
abwhitley <a.blakewhitley@gmail.com>
2016-08-24 15:15:59 -0400

    Initial Commit

---
+++ Counter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Counter
+//
+//  Created by Austins Work on 8/24/16.
+//  Copyright © 2016 AustinsIronYard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
